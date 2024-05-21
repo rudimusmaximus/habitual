a7bd685815ce707e911cc800a7979b295d44019d
Raul Flores, Jr. MBA "rudy <rudy@redcrowconsulting.com>
2024-05-20 23:48:14 -0500

    feat: v0.0.1-setup.2 a few example tailwind uses

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,7 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1 class="title">About this app</h1>
 
 	<p>
 		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
@@ -24,3 +24,21 @@
 		using it with JavaScript disabled!
 	</p>
 </div>
+
+<style lang="postcss">
+	/* This is how you can use the postcss preprocessor for tailwind */
+
+	.title {
+		@apply text-blue-600;
+	}
+
+	/* Change text color to red on hover */
+	.title:hover {
+		@apply text-red-600;
+	}
+
+	/* Apply different text sizes based on screen size */
+	.title {
+		@apply text-lg md:text-xl lg:text-2xl;
+	}
+</style>
