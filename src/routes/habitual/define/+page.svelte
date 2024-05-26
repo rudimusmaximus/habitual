0ad53571d273e9dd30778575336d6eda5173577d
Raul Flores, Jr. MBA "rudy <rudy@redcrowconsulting.com>
2024-05-26 00:16:01 -0500

    feat: v0.1.3-vercel.0 wip ...
     - cleaned up missing links and added placeholder pages
    
     - updated README for lessons and current state
    
     - completed vercel and cloud integrations via web
    
     - schema is on cloud now
    
     - setting are so that deleted branches on gh result
    deleted branches on cloud db as a sort of automatic clean up

---
+++ src/routes/habitual/define/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>Define</title>
+  <meta name="description" content="Define action for new habit application set" />
+</svelte:head>
+
+<section>
+  <h1>TODO when database ready.</h1>
+</section>
+
+<style>
+  h1 {
+    width: 100%;
+  }
+</style>
---
+++ src/routes/habitual/review/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>Review</title>
+  <meta name="description" content="Review action for new habit application set" />
+</svelte:head>
+
+<section>
+  <h1>TODO when database ready.</h1>
+</section>
+
+<style>
+  h1 {
+    width: 100%;
+  }
+</style>
